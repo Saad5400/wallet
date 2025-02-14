3c0a2aef34ce73e2b34694bedfc775b70bb8c983
Saad5400 <sdbtwa@gmail.com>
2025-02-14 13:24:43 +0300

    login page

--- resources/js/Pages/Welcome/Email/Page.tsx
+++ resources/js/Pages/Welcome/Email/Page.tsx
@@ -1,9 +1,15 @@
 import { Button } from '@/components/ui/button';
 import Layout from '@/Pages/Welcome/Email/Layout';
-import { Head, Link } from '@inertiajs/react';
+import { Head, Link, useForm } from '@inertiajs/react';
 import { Icon } from "@iconify/react";
+import { Label } from '@/components/ui/label';
+import { Input } from '@/components/ui/input';
 
 export default function Default() {
+    const { data, setData, post, processing, errors, reset } = useForm({
+        email: '',
+    });
+
     return (
         <Layout>
             <Head title="هلا" />
@@ -12,7 +18,22 @@ export default function Default() {
                     الدخول
                 </h5>
 
-                
+                <div className='flex flex-col gap-2'>
+                    <Label htmlFor='email' className='self-start'>
+                        البريد الإلكتروني
+                    </Label>
+                    <Input
+                        id='email'
+                        name='email'
+                        type='email'
+                        dir='ltr'
+                        placeholder='example@xyz.com'
+                        className='placeholder:text-muted'
+                        value={data.email}
+                        onChange={e => setData('email', e.target.value)}
+                    />
+                    {errors.email && <small className='self-end text-destructive'>{errors.email}</small>}
+                </div>
 
                 <div className='flex flex-col items-start'>
                     <Button size={'wide'}>
