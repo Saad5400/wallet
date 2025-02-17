f493267a3b9ed0d88baa6588e0389f6c00999891
Saad5400 <sdbtwa@gmail.com>
2025-02-17 18:09:14 +0300

    balance

--- app/Models/Account.php
+++ app/Models/Account.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
 use Stancl\Tenancy\Database\Concerns\BelongsToTenant;
 
@@ -22,5 +23,12 @@ class Account extends Model
     public function outgoingRecords()
     {
         return $this->hasMany(Record::class, 'source_account_id');
+    }
+
+    public function balance(): Attribute
+    {
+        return Attribute::make(function () {
+            return $this->incomingRecords->sum('amount') - $this->outgoingRecords->sum('amount');
+        });
     }
 }
--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Stancl\Tenancy\Contracts\Tenant as TenantContract;
 use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
 
@@ -12,5 +13,32 @@ class Tenant extends BaseTenant implements TenantContract
     public function users()
     {
         return $this->morphedByMany(User::class, User::$morphName, User::$tenantPivotTable, User::$tenantForeignKey, 'tenantable_id');
+    }
+
+    public function accounts()
+    {
+        return $this->hasMany(Account::class);
+    }
+
+    public function categories()
+    {
+        return $this->hasMany(Category::class);
+    }
+
+    public function subCategories()
+    {
+        return $this->hasMany(SubCategory::class);
+    }
+
+    public function records()
+    {
+        return $this->hasMany(Record::class);
+    }
+
+    public function balance(): Attribute
+    {
+        return Attribute::make(function () {
+            return $this->accounts->sum('balance');
+        });
     }
 }
