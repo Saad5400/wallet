640a05ecf3cd1d5e5f3da269d424940f7708bbf9
Saad5400 <sdbtwa@gmail.com>
2025-02-28 05:26:52 +0300

    eh

--- resources/js/Pages/MainLayout.tsx
+++ resources/js/Pages/MainLayout.tsx
@@ -1,24 +1,22 @@
 import React from 'react';
 
-import ProgressBar from './ProgressBar';
-import ViewTransition from './ViewTransition';
-import { useIsMobile } from '@/hooks/use-mobile';
+import {useIsMobile} from '@/hooks/use-mobile';
 import AndroidMockup from '@/components/mockup/AndroidMockup';
 
-export default function MainLayout({ children }: { children: React.ReactNode }): JSX.Element {
+export default function MainLayout({children}: { children: React.ReactNode }): JSX.Element {
     const isMobile = useIsMobile();
 
     return (
         <>
-            <ViewTransition />
             {isMobile ?
                 <>
-                    <ProgressBar />
                     {children}
                 </>
                 :
-                <div className='flex justify-center items-center p-16 min-h-screen bg-slate-700' style={{ viewTransitionName: 'phone-mockup' }}>
-                    <AndroidMockup screenWidth={350} className='drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)]' statusbarColor='#202020' frameColor='#202020' noRoundedScreen={true}>
+                <div className='flex justify-center items-center p-16 min-h-screen bg-slate-700'
+                     style={{viewTransitionName: 'phone-mockup'}}>
+                    <AndroidMockup screenWidth={350} className='drop-shadow-[0_35px_35px_rgba(0,0,0,0.25)]'
+                                   statusbarColor='#202020' frameColor='#202020' noRoundedScreen={true}>
                         {children}
                     </AndroidMockup>
                 </div>
--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -2,11 +2,29 @@ import '../css/app.css';
 import '../css/animations.css';
 import './bootstrap';
 
-import { createInertiaApp } from '@inertiajs/react';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createRoot, hydrateRoot } from 'react-dom/client';
+import {createInertiaApp} from '@inertiajs/react';
+import {resolvePageComponent} from 'laravel-vite-plugin/inertia-helpers';
+import {createRoot, hydrateRoot} from 'react-dom/client';
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+
+if (document.startViewTransition) {
+    document.addEventListener("inertia:start", () => {
+        document.startViewTransition(() => {
+            return new Promise<void>((resolve) => {
+                document.addEventListener(
+                    "inertia:finish",
+                    () => {
+                        setTimeout(() => {
+                            resolve();
+                        });
+                    },
+                    {once: true},
+                );
+            });
+        });
+    });
+}
 
 createInertiaApp({
     title: (title) => `${title} - ${appName}`,
@@ -15,7 +33,7 @@ createInertiaApp({
             `./Pages/${name}.tsx`,
             import.meta.glob('./Pages/**/*.tsx'),
         ),
-    setup({ el, App, props }) {
+    setup({el, App, props}) {
         if (import.meta.env.SSR) {
             hydrateRoot(el, <App {...props} />);
             return;
